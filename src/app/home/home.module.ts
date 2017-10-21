e6617a251f93cf9d040c66443860efcc70368cc2
Alejandro Lora <alejandrofpo@gmail.com>
2017-10-22 00:20:20 +0100

    INTERNAL: Added piglatin pipe to home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 import { HomeRoutes } from './home.routing';
 
+import { PigLatinPipe } from '@pipes/index';
+
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule,
         HomeRoutes
     ],
     declarations: [
-        HomeComponent
+        HomeComponent,
+        PigLatinPipe
     ]
 })
 
